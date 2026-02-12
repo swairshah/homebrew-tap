c9bf11e8018215901675a71fa1969690e3771832
swairshah <swairshah@gmail.com>
2026-02-12 12:33:57 -0800

    Update Bezel to notarized build

--- Casks/bezel.rb
+++ Casks/bezel.rb
@@ -1,6 +1,6 @@
 cask "bezel" do
   version "0.1.0"
-  sha256 "72a695cdbf8e9d72dafbd30d1d1b835ad041068f34a99d4a46f86829a6c9814c"
+  sha256 "002561b3e1128f9a9fa94ff57e0c9b0c0999b051ca738fa7536fe3726812157e"
 
   url "https://github.com/swairshah/Bezel/releases/download/v#{version}/Bezel-#{version}.zip"
   name "Bezel"
