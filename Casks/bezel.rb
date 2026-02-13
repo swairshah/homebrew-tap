9e2486ec83e40aff5b2fe946c3705a42d2c1a508
swairshah <swairshah@gmail.com>
2026-02-12 18:41:12 -0800

    Update Bezel to v0.1.2 - timer controls

--- Casks/bezel.rb
+++ Casks/bezel.rb
@@ -1,6 +1,6 @@
 cask "bezel" do
-  version "0.1.1"
-  sha256 "e563dd0044b9730bc7a65bc54a3c3a5f94273bac2f031a4d3c2a666811a8f2a7"
+  version "0.1.2"
+  sha256 "6c000d1b67119a90ac7fb49d55d7e4fa1f1cd29c1410da52661b8a589ee87bbb"
 
   url "https://github.com/swairshah/Bezel/releases/download/v#{version}/Bezel-#{version}.zip"
   name "Bezel"
