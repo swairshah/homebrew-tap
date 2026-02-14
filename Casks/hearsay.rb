c0f7b3d425cf48732f25b02046d05be88b243208
swairshah <swairshah@gmail.com>
2026-02-13 18:48:47 -0800

    Add Hearsay v1.0 - local speech-to-text app

---
+++ Casks/hearsay.rb
@@ -0,0 +1,34 @@
+cask "hearsay" do
+  version "1.0"
+  sha256 "9775bf69d0612350cf6793b151a8ce699ee48936afd163daae4a35cbad6ed27a"
+
+  url "https://github.com/swairshah/hearsay/releases/download/v#{version}/Hearsay-#{version}.dmg"
+  name "Hearsay"
+  desc "Local speech-to-text transcription using Qwen3-ASR"
+  homepage "https://github.com/swairshah/hearsay"
+
+  depends_on macos: ">= :ventura"
+
+  app "Hearsay.app"
+
+  postflight do
+    system_command "/usr/bin/open", args: [staged_path.join("Hearsay.app")]
+  end
+
+  zap trash: [
+    "~/Library/Application Support/Hearsay",
+    "~/Library/Preferences/com.swair.hearsay.plist",
+    "~/Library/Caches/com.swair.hearsay",
+  ]
+
+  caveats <<~EOS
+    Hearsay requires Microphone and Accessibility permissions.
+    
+    On first launch, you'll be prompted to download a speech recognition model.
+    Models are stored in ~/Library/Application Support/Hearsay/Models/
+
+    Usage:
+      • Hold Right Option (⌥) to record, release to transcribe
+      • Toggle mode: Right Option + Space to start, Space/Escape to stop
+  EOS
+end
