c73eeb971c4dd3a639f669b9f3b1dba555c1c95b
swairshah <swairshah@gmail.com>
2026-02-18 16:23:38 -0800

    Update hearsay to 1.0.8

--- Casks/hearsay.rb
+++ Casks/hearsay.rb
@@ -1,6 +1,6 @@
 cask "hearsay" do
-  version "1.0.7"
-  sha256 "d6353691e408c01e1b2af43cd83450a10a092d96e7884d7be67f3f33d670d32d"
+  version "1.0.8"
+  sha256 "3bf558f71fb13144f9cd243a3deb7537a491beb9e96102182e84eeda79600bd5"
 
   url "https://github.com/swairshah/hearsay/releases/download/v#{version}/Hearsay-#{version}.dmg"
   name "Hearsay"
