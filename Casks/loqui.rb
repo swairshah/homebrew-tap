8828e5fca1da13e4be4cdf89363f4674bbe05c45
swairshah <swairshah@gmail.com>
2026-05-04 12:20:04 -0700

    Update loqui to 1.4.0

--- Casks/loqui.rb
+++ Casks/loqui.rb
@@ -1,6 +1,6 @@
 cask "loqui" do
-  version "1.3.0"
-  sha256 "2fb7aa848194eb502b8cc828ceabc9d78bd15cd8c665e0d21691f9a40990fafb"
+  version "1.4.0"
+  sha256 "219d7e261496643090e04e23633d99a3b8c48d11221598a7982c0efa646601aa"
 
   url "https://github.com/swairshah/Loqui/releases/download/v#{version}/Loqui-#{version}.zip"
   name "Loqui"
