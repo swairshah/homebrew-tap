bf56e0da03e80433fd0bbaf87ef20b47beed0faf
swairshah <swairshah@gmail.com>
2026-08-14 03:52:37 -0700

    Update pitalk to 1.3.2

--- Casks/pitalk.rb
+++ Casks/pitalk.rb
@@ -1,13 +1,13 @@
 cask "pitalk" do
-  version "1.3.1"
-  sha256 "1dde2dce4d24138ea86fdef23ac335bb57d3704b5631fa0b2aa01696931ce0f8"
+  version "1.3.2"
+  sha256 "2ae10bb30cd0f5c8e106a73f08b42c49830b4a939335e938396f590657fb7c5d"
 
   url "https://github.com/swairshah/PiTalk/releases/download/v#{version}/PiTalk-#{version}.dmg"
   name "PiTalk"
   desc "Voice interface for pi coding agent with TTS feedback"
   homepage "https://github.com/swairshah/PiTalk"
 
-  depends_on macos: ">= :ventura"
+  depends_on macos: :ventura
 
   app "PiTalk.app"
 
